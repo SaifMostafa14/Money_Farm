dcf79e5d2c6b1e74478c2ff1f9022957e3950019
nko-wat <67723227+nko-wat@users.noreply.github.com>
2022-05-02 04:36:54 -0400

    Update SpendingPage.js

--- Production/src/SpendingPage.js
+++ Production/src/SpendingPage.js
@@ -1,225 +1,197 @@
-import './spendingpage.css';
+import '../spendingpage.css';
 import * as B from 'react-bootstrap';
-import {
-    LineChar, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, PieChart,
+import { LineChar, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, PieChart,
     Pie,
-    Cell,
-} from 'recharts';
+    Cell, } from 'recharts';
 import {Container, Navbar, Table, Card} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import React from "react";
 
 
-let globalFood = 0;
-let globalTravel = 0;
+
+let globalFood =0;
+let globalTravel =0;
 
 
 function totalSum(balance, transactions) {
-    console.log(transactions);
-    let transCapMap = new Map();
-    let bMap = new Map();
-    let checkingBal = "";
-    let savingBal = "";
-    let totsum = 0;
-    let moneySpent = 0;
-    let travelsum = 0;
-    let foodsum = 0;
-    let shopsum = 0;
-    let recsum = 0;
-    let miscsum = 0;
-
+  console.log( transactions);
+  let transCapMap = new Map();
+  let bMap = new Map();
+  let checkingBal = "" ;
+  let savingBal = "" ;
+  let totsum =0;
+  let moneySpent = 0;
+  let travelsum =0;
+  let foodsum=0;
+  let shopsum=0;
+  let recsum=0;
+  let miscsum = 0;
+   
     //loads Plaid values k=name and v=account ID into bMap
     for (const user of balance.Balance.accounts.values()) {
-        let k = user.name;
-        let v = user.account_id;
-        let m = user.balances.available;
-        bMap.set(k, v);
-        if (k === 'Plaid Checking') {
-            checkingBal = m;
-            console.log('plaid checl');
+      let k = user.name;
+      let v = user.account_id;
+      let m = user.balances.available;
+      bMap.set(k, v);
+      if(k === 'Plaid Checking'){
+          checkingBal = m;
+          console.log('plaid checl');
 
-        } else if (k === 'Plaid Saving') {
-            savingBal = m;
-        } else {
-            console.log("plaid sav");
-        }
-        //console.log(user.account_id);
+      }else if(k === 'Plaid Saving'){
+          savingBal = m;
+      }else{
+          console.log("plaid sav");
+      }
+      //console.log(user.account_id);
 
-    }
-    ;
+    };
 
-    for (const t of transactions.values()) {
-        let k = t.account_id;
-        let v = (t.amount * -1); //posive values are widthdrawls, negative values are deposits -> negate
-        let c = t.category;
-        let cid = t.category_id;
-        let type = t.category_id.substring(0, 3)
-        console.log('Trans Date: ' + t.date);
+  for (const t of transactions.values()) {
+      let k = t.account_id;
+      let v = (t.amount * -1); //posive values are widthdrawls, negative values are deposits -> negate
+      let c = t.category;
+      let cid = t.category_id;
+      let type = t.category_id.substring(0,3)
+      console.log('Trans Date: ' + t.date);
+      
+      if(k === bMap.get('Plaid Checking')){
+          totsum = totsum += v;
+          switch(type){
+              case '220':
+                  travelsum = travelsum += v;
+                  console.log('Travel expenses: ' + travelsum);
+                  break;
 
-        if (k === bMap.get('Plaid Checking')) {
-            totsum = totsum += v;
-            switch (type) {
-                case '220':
-                    travelsum = travelsum += v;
-                    console.log('Travel expenses: ' + travelsum);
-                    break;
+              case '130':
+                  foodsum = foodsum += v;
+                  console.log('Food expenses: ' + foodsum);
+                  break; 
+              case '190':
+                  shopsum = shopsum += v;
+                  console.log('Shopping expenses: ' + shopsum);
+                  break;
 
-                case '130':
-                    foodsum = foodsum += v;
-                    console.log('Food expenses: ' + foodsum);
-                    break;
-                case '190':
-                    shopsum = shopsum += v;
-                    console.log('Shopping expenses: ' + shopsum);
-                    break;
+              case '170':
+                  recsum = recsum += v;
+                  console.log('Recreation expenses: ' + recsum);
+                  break;  
 
-                case '170':
-                    recsum = recsum += v;
-                    console.log('Recreation expenses: ' + recsum);
-                    break;
+              case '210' || '160':
+                  miscsum = miscsum += v;
+                  console.log('Miscellaneous expenses: ' + miscsum);
+                  break;  
+                 
+          }
 
-                case '210' || '160':
-                    miscsum = miscsum += v;
-                    console.log('Miscellaneous expenses: ' + miscsum);
-                    break;
+          if(v < 0){
+              moneySpent = moneySpent += v;
+              
+          }
 
-            }
+      }
+      transCapMap.set(c, cid);
+      
 
-            if (v < 0) {
-                moneySpent = moneySpent += v;
+    };console.log(totsum);console.log('Money Spent: ' + moneySpent);
+    
+   //console.log( 'Testing transCap: ' + transCapMap.values() );
 
-            }
-
-        }
-        transCapMap.set(c, cid);
-
-
-    }
-    ;console.log(totsum);
-    console.log('Money Spent: ' + moneySpent);
-
-    //console.log( 'Testing transCap: ' + transCapMap.values() );
-
-    for (let key of transCapMap.keys()) {
+    for (let key of transCapMap.keys()){
         // console.log('TRansMap keys: ' + key)
     }
-
+    
     globalFood = foodsum;
     globalTravel = travelsum;
+  
+  return(
+    
+      
+      //bMap
+      
+      <Container id = 'stuffFromTrans'>
+          <div className="row mt-3" id="budget-functions">
+                  <div className="row mt-3" id="budget-functions">
+                            
+                            <div className="col-sm" id="budget-col">
+                                <div className='alert alert-primary'>
+                                        <span className='m'> Checking Account Balance: ${checkingBal} </span>
+                                </div>
+                            </div>
+                      
+                            <div className='col-sm' id="remaining-col">
+                                <div className='alert alert-primary '>
+                                        <span className='col-sm'> Savings Account Balance: ${savingBal} </span>
+                                </div>
+                            </div>
+                      
+                  </div> 
+              
+          </div>         
 
-    return (
+      </Container>
+      
 
+      
+  );
+     
+   
+};
 
-        //bMap
-
-        <Container id='stuffFromTrans'>
-            <div className="row mt-3" id="budget-functions">
-                <div className="row mt-3" id="budget-functions">
-
-                    <div className="col-sm" id="budget-col">
-                        <div className='alert alert-primary'>
-                            <span className='m'> Checking Account Balance: ${checkingBal} </span>
-                        </div>
-                    </div>
-
-                    <div className='col-sm' id="remaining-col">
-                        <div className='alert alert-primary '>
-                            <span className='col-sm'> Savings Account Balance: ${savingBal} </span>
-                        </div>
-                    </div>
-
-                </div>
-
-            </div>
-
-        </Container>
-
-
-    );
-
-
-}
-
-function enterData() {
-
-    console.log('GLOBAL Food: ' + globalFood);
-    console.log('GLOBAL TRAVEL: ' + globalTravel);
-    let globalShopping = 120;
-    let globalFun = 100;
-    let globalMisc = 70;
-    const data = [
+function enterData (){
+  
+  console.log('GLOBAL Food: ' + globalFood);
+  console.log('GLOBAL TRAVEL: ' + globalTravel);
+  let globalShopping = Math.floor(Math.random() * 300);
+  let globalFun = Math.floor(Math.random() * 900) + 100;
+  let globalMisc = Math.floor(Math.random() * 500) + 40;
+  const data = [
         {
             name: 'Food/Eating Out',
-            spending: (globalFood * -1),
+            Total: (globalFood.toFixed(2) * -1),
         },
         {
             name: 'Groceries',
-            spending: (globalShopping),
+            Total: (globalShopping),
         },
         {
             name: 'Transportation',
-            spending: (globalTravel),
+            Total: (globalTravel.toFixed(2)),
         },
         {
             name: 'Entertainment',
-            spending: globalFun,
+            Total: globalFun,
         },
-        {
+         {
             name: 'Misc.',
-            spending: globalMisc,
+            Total: globalMisc,
         },
     ];
 
-    return (
+    return(
 
-        data
+      data
 
     );
 }
 
 
-function bara(props) {
+function bara (props) {
 
     return (
-        <>
-            <div id='spending-css'>
-                <Navbar bg="dark" variant="dark" className='nav justify-content-center'>
-                    <Container className=''>
-                        <Navbar.Brand>GreenPocket</Navbar.Brand>
+        
 
-                        <Link className=" nav nav-pills nav-fill nav-link" to="/budget">Budget</Link>
-                        <Link className="nav nav-pills nav-fill nav-link" to="/connect_bank">Conect Bank</Link>
-                        <Link className="nav nav-pills nav-fill nav-link" to="/SpendingPage">Spending</Link>
+        
 
-                    </Container>
-                </Navbar>
-                <h4 className="content-align-center" id="getCategoryTotals">
-                    {totalSum(props.balance, props.transactions)}
-                </h4>
-                <div>
+        <div id='spending-css'>
+              {props.transactions ? (
 
-                    {/*<ReactBootStrap.Navbar bg="success" variant="dark">*/}
-                    {/*    <ReactBootStrap.Container>*/}
-                    {/*        <ReactBootStrap.Navbar.Brand href="#home">GreenPocket</ReactBootStrap.Navbar.Brand>*/}
-                    {/*        <ReactBootStrap.Nav className="me-auto">*/}
-                    {/*            <ReactBootStrap.Nav.Link href="#home">Home</ReactBootStrap.Nav.Link>*/}
-                    {/*            <ReactBootStrap.Nav.Link href="#features">logout</ReactBootStrap.Nav.Link>*/}
-                    {/*        </ReactBootStrap.Nav>*/}
-                    {/*    </ReactBootStrap.Container>*/}
-                    {/*</ReactBootStrap.Navbar>*/}
-
-                    {/*<div id="spending-card">*/}
-
-                    {/*    <p id="spending-p"> Current Spending</p>*/}
-
-                    {/*    <h1 id="spending-h1">$5432.12</h1>*/}
-
-
-                    {/*</div>*/}
-
-                </div>
-                <div id='spending-barr'>
-                    <ResponsiveContainer classNmae='content-align-center' aspect={1.5}>
+        <div className="col-sm" id="budget-col">
+            <h4 className="content-align-center" id="getCategoryTotals" >
+                {totalSum(props.balance, props.transactions)}
+            </h4>
+                
+                <div  id='spending-barr'>
+                <ResponsiveContainer className='content-align-center' aspect=1.52}>
                         <BarChart
                             width={250}
                             height={80}
@@ -230,69 +202,44 @@ function bara(props) {
                                 left: 20,
                                 bottom: 5,
                             }}
-                            barSize={30}
+                            barSize={20}
                         >
                             <XAxis dataKey="name" scale="point" padding={{left: 10, right: 10}}/>
                             <YAxis/>
                             <Tooltip/>
                             <Legend/>
                             <CartesianGrid strokeDasharray="3 3"/>
-                            <Bar dataKey="spending" fill="green" background={{fill: 'gray'}}/>
+                            <Bar dataKey="Total" fill="green" background={{fill: 'gray'}}/>
                         </BarChart>
                     </ResponsiveContainer>
-                </div>
-
-
-                {/*<div id='spending-pie'>
-                    <ResponsiveContainer width="95%" aspect={2}>
-                        <PieChart width={70} height={90}>
-                            <Pie
-                                data={data2}
-                                cx="50%"
-                                cy="50%"
-                                labelLine={false}
-                                label={renderCustomizedLabel}
-                                outerRadius={150}
-                                fill="#8884d8"
-                                dataKey="value"
-                            >
-                                {data.map((entry, index) => (
-                                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]}/>
-                                ))}
-                            </Pie>
-                        </PieChart>
-                                </ResponsiveContainer>
-
-
-                                </div>*/}
-
-                <div id='spending-table-div'>
-                    <Container className='col-12 col-lg-8 col-xxl-9 d-flex my-4' id='spending-table'>
-                        <Card border="dark" style={{width: '70rem'}} id='spending-table-card'>
-                            <Card.Header as='h4'>Recent Transactions
-                                <Container id="transaction-table-container">
-                                    <B.Form.Control type="date" name='start_date' value={props.startDate}
-                                                    onChange={e => props.setStartDate(e.target.value)}/>
-                                    <B.Form.Control type="date" name='end_date' value={props.endDate}
-                                                    onChange={e => props.setEndDate(e.target.value)}/>
-
-                                    <B.Button variant="secondary" onClick={props.getTransactionsFunction}>Get
-                                        Transactions</B.Button>
-                                </Container>
-
-                            </Card.Header>
-                            <Card.Body>
-                                <Table striped bordered hover>
-                                    <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Date</th>
-                                        <th>Transaction</th>
-                                        <th>Amount</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    {props.transactions.map(t => (
+                        </div>
+            
+            <div id='spending-table-div'>
+                <Container className='col-12 col-lg-8 col-xxl-9 d-flex my-4' id='spending-table'>
+                    <Card border="dark" style={{ width: '70rem' }} id='spending-table-card'>
+                        <Card.Header as='h4'>Recent Transactions
+                            <Container id="transaction-table-container" >
+                                <B.Form.Control type="date" name='start_date' value={props.startDate}
+                                    onChange={e => props.setStartDate(e.target.value)}/>
+                                <B.Form.Control type="date" name='end_date' value={props.endDate}
+                                    onChange={e => props.setEndDate(e.target.value)}/>
+                                
+                                <B.Button variant="secondary" onClick={props.getTransactionsFunction}>Get Transactions</B.Button>
+                            </Container>
+                            
+                        </Card.Header>
+                        <Card.Body>
+                            <Table striped bordered hover >
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Date</th>
+                                    <th>Transaction</th>
+                                    <th>Amount</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {props.transactions.map(t => (
                                         <tr>
                                             <td>{t.date}</td>
                                             <td>{t.name}</td>
@@ -301,16 +248,37 @@ function bara(props) {
                                         </tr>
                                     ))}
 
-                                    </tbody>
-                                </Table>
-                            </Card.Body>
-                        </Card>
+                            </tbody>
+                            </Table>  
+                        </Card.Body>
+                    </Card> 
+                    
+                </Container>  
+            </div>  
 
-                    </Container>
-                </div>
-            </div>
-        </>
+
+        </div>
+    ) : 
+
+        <div className="p-5 mb-4 bg-light rounded-3">
+            <Container className='py-5' id='new-user container'>
+                <h1 className='fw-bold'>Great! Now let's check out your transactions!</h1>
+                <p className='col-md-8'>
+                    Click the button below to see this month's transactions
+                </p>
+                <B.Button className='btn btn-lg' variant='primary'onClick={props.getTransactionsFunction}>Get Transactions</B.Button>
+            </Container>
+            
+        </div> 
+
+    }
+            <br></br>
+         <footer className="footerHP">
+                    <p>Copyrights @ GREEN POCKET</p>
+        </footer>
+        </div>
     );
 }
 
 export default bara;
+
